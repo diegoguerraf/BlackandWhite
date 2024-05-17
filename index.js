1ea8a48ecb12f9a868f5eb99ad669019e43774e7
diegoguerraf <guerra.farias.diego@gmail.com>
2024-05-17 18:12:46 -0400

    primera version

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './routes/routes.js'
+const app = express();
+const PORT = process.env.PORT || 3000
+
+//carpeta publica
+/* app.use(express.static("assets")); */
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes);
+
+app.listen(PORT, ()=>console.log(`servidor activo en http://localhost:${PORT}`));
